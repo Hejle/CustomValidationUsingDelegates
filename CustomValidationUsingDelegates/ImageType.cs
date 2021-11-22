774c4281e04d945a79c6f74dae4087406a0baaa8
Hejle <joachim-dk@hotmail.com>
2021-11-22 23:04:03 +0100

    Added specific default Validation example

--- CustomValidationUsingDelegates/ImageType.cs
+++ CustomValidationUsingDelegates/ImageType.cs
@@ -6,6 +6,7 @@ public class ImageType : ICustomValidationField
 {
     [Required]
     [CustomValidation]
+    [ImageTypeValidation]
     public string? Value { get; set; }
 
     public Func<object, bool>[]? CustomValidationRules { get; set; }
---
+++ CustomValidationUsingDelegates/ImageTypeValidationAttribute.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CustomValidationUsingDelegates;
+
+public class ImageTypeValidationAttribute : ValidationAttribute
+{
+    protected override ValidationResult? IsValid(object? value, ValidationContext validationContext)
+    {
+        if ((value as string) is "Face" or "Leg" or "Arm" or "Foot")
+        {
+            return ValidationResult.Success;
+        }
+        return new ValidationResult($"{value} in Image Type is neither Face, Leg, Arm or Foot");
+    }
+}
